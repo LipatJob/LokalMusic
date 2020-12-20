a463dae55048edee5846bfb461d835ab3163be4f
LipatJob <35028794+LipatJob@users.noreply.github.com>
2020-12-21 00:42:15 +0800

    Created NavigationHelper

---
+++ LokalMusic/_Code/Helpers/NavigationHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LokalMusic._Code.Helpers
+{
+    public class NavigationHelper
+    {
+        public static void Redirect(string url)
+        {
+            HttpContext.Current.Response.Redirect(url);
+        }
+
+        public static object GetRouteValue(string key)
+        {
+            return HttpContext.Current.Request.RequestContext.RouteData.Values[key];
+        }
+
+        public static string QueryString(string key)
+        {
+            return HttpContext.Current.Request.QueryString[key];
+        }
+    }
+}
