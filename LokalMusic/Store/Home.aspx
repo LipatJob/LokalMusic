95e0198db6920f9527e2b942395395ac8c849a2f
gn-grc <genejogarcia.gg@gmail.com>
2020-12-24 22:56:52 +0800

    Added files for Home page

---
+++ LokalMusic/Store/Home.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Template/StoreLayout.master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="LokalMusic.Store.Home" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+</asp:Content>
---
+++ LokalMusic/_Code/Presenters/Store/HomePresenter.cs
@@ -0,0 +1,15 @@
+﻿using LokalMusic._Code.Views.Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LokalMusic._Code.Repositories.Store
+{
+    public class HomePresenter
+    {
+
+        private IHomeViewModel view;
+
+    }
+}
