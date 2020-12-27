53edfb38a1d4ab3586ebbe30639c67768c59fff5
gn-grc <genejogarcia.gg@gmail.com>
2020-12-27 13:57:37 +0800

    prepared query strings for artist, album, track record retrieval

--- LokalMusic/_Code/Repositories/ProductRepository.cs
+++ LokalMusic/_Code/Repositories/ProductRepository.cs
@@ -3,10 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+// Author - Gene Garcia
+
 namespace LokalMusic._Code.Repositories
 {
     public class ProductRepository
     {
-        
+        public void GetCompleteProductCatalogue()
+        {
+            string query = "SELECT * " + 
+                           "FROM Track " +
+                           "INNER JOIN Product " +
+                           "ON Track.TrackId = Product.ProductId " +
+                           "INNER JOIN Album " +
+                           "ON Track.AlbumId = Album.AlbumId " +
+                           "INNER JOIN ArtistInfo " + 
+                           "ON Album.UserId = ArtistInfo.UserId " +
+                           "WHERE Product.ProductStatusId = (SELECT ProductStatusId FROM ProductStatus WHERE StatusName = 'LISTED') ";
+        }
+
+        public void GetProductByArtist(/*IArtistModel or Id or Name*/)
+        {
+            string query = "SELECT * " +
+                           "FROM Track " +
+                           "INNER JOIN Product " +
+                           "ON Track.TrackId = Product.ProductId " +
+                           "INNER JOIN Album " +
+                           "ON Track.AlbumId = Album.AlbumId " +
+                           "INNER JOIN ArtistInfo " +
+                           "ON Album.UserId = ArtistInfo.UserId " +
+                           "WHERE Product.ProductStatusId = (SELECT ProductStatusId FROM ProductStatus WHERE StatusName = 'LISTED') " +
+                           "AND ArtistInfo.UserId = 0 " +
+                           "AND ArtistInfo.ArtistName = ' '";
+        }
+
+        public void GetProductByAlbum(/*IAlbum or Id or Name*/)
+        {
+            string query = "SELECT * " +
+                           "FROM Track " +
+                           "INNER JOIN Product " +
+                           "ON Track.TrackId = Product.ProductId " +
+                           "INNER JOIN Album " +
+                           "ON Track.AlbumId = Album.AlbumId " +
+                           "INNER JOIN ArtistInfo " +
+                           "ON Album.UserId = ArtistInfo.UserId " +
+                           "WHERE Product.ProductStatusId = (SELECT ProductStatusId FROM ProductStatus WHERE StatusName = 'LISTED') " +
+                           "AND Album.AlbumName = ' ' " +
+                           "AND Album.Album.Id = 0";
+        }
+
+        public void GetProductByTrack(/*ITrack or Id or Name*/)
+        {
+            string query = "SELECT * " +
+                           "FROM Track " +
+                           "INNER JOIN Product " +
+                           "ON Track.TrackId = Product.ProductId " +
+                           "INNER JOIN Album " +
+                           "ON Track.AlbumId = Album.AlbumId " +
+                           "INNER JOIN ArtistInfo " +
+                           "ON Album.UserId = ArtistInfo.UserId " +
+                           "WHERE Product.ProductStatusId = (SELECT ProductStatusId FROM ProductStatus WHERE StatusName = 'LISTED') " +
+                           "AND Track.TrackName = ' ' " +
+                           "AND Track.TrackId = 0";
+        }
+
     }
 }
