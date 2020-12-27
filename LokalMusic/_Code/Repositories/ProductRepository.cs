3f6ace22a2f821b903f5745d65799c596f7e0076
gn-grc <genejogarcia.gg@gmail.com>
2020-12-27 13:44:34 +0800

    removed home repo, and create blank product repo

--- LokalMusic/_Code/Repositories/ProductRepository.cs
+++ LokalMusic/_Code/Repositories/ProductRepository.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace LokalMusic._Code.Repositories.Store
+namespace LokalMusic._Code.Repositories
 {
-    public class HomeRepository
+    public class ProductRepository
     {
+        
     }
 }
