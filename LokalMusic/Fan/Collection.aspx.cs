8e15eb71d08bf9a6a750c7207d248a7d97871612
LipatJob <35028794+LipatJob@users.noreply.github.com>
2020-12-29 01:01:08 +0800

    partial implementation of collections

---
+++ LokalMusic/Fan/Collection.aspx.cs
@@ -0,0 +1,21 @@
+﻿using LokalMusic._Code.Models.Fan;
+using LokalMusic._Code.Presenters.Fan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LokalMusic.Fan
+{
+    public partial class Collection : System.Web.UI.Page
+    {
+        private CollectionPresenter presenter;
+        public ICollection<CollectionItem> Model { get { return presenter.model;  } }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LokalMusic/_Code/Models/Fan/CollectionItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LokalMusic._Code.Models.Fan
+{
+    public class CollectionItem
+    {
+        public string CoverLink { get; set; }
+        public string ProductName { get; set; }
+        public string ArtistName { get; set; }
+        public string ProductType { get; set; }
+        public int ArtistId { get; set; }
+        public int TrackId { get; set; }
+        public int AlbumId { get; set; }
+
+        public string GetUrl 
+        { 
+            get 
+            {
+                if (ProductType == "ALBUM")
+                {
+                    return $"~/Store/{ArtistId}/{AlbumId}";
+                }
+                else
+                {
+                    return $"~/Store/{ArtistId}/{AlbumId}/{TrackId}";
+                }
+            } 
+        }
+    }
+}
