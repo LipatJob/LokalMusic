b3535986c0dabb90bc7716f2fd58297b982aca4b
gn-grc <genejogarcia.gg@gmail.com>
2020-12-28 19:48:16 +0800

    added constructors

--- LokalMusic/_Code/Models/Products/Album.cs
+++ LokalMusic/_Code/Models/Products/Album.cs
@@ -7,6 +7,21 @@ namespace LokalMusic._Code.Models.Products
 {
     public class Album
     {
+        public Album()
+        {
+
+        }
+
+        public Album(int albumId, int albumCoverId, string albumName, string description, DateTime dateReleased, int userId)
+        {
+            AlbumId = albumId;
+            AlbumCoverId = albumCoverId;
+            AlbumName = albumName;
+            Description = description;
+            DateReleased = dateReleased;
+            UserId = userId;
+        }
+
         public int AlbumId { get; set; }
         public int AlbumCoverId { get; set; }
         public string AlbumName { get; set; }
