fd123b60cc57e2a610e18c74b86825d8bca29923
gn-grc <genejogarcia.gg@gmail.com>
2020-12-31 13:37:14 +0800

    implemented display tracks page

--- LokalMusic/Store/TracksPage.aspx.cs
+++ LokalMusic/Store/TracksPage.aspx.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using LokalMusic._Code.Models.Products;
+using LokalMusic._Code.Presenters.Store;
+using LokalMusic._Code.Repositories;
+using LokalMusic._Code.Views.Store;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,11 +11,21 @@ using System.Web.UI.WebControls;
 
 namespace LokalMusic.Store
 {
-    public partial class TracksPage : System.Web.UI.Page
+    public partial class TracksPage : System.Web.UI.Page, ITracksPage
     {
+        private TracksPagePresenter presenter;
+        public List<Track> tracks;
+        public TracksPage()
+        {
+            this.presenter = new TracksPagePresenter(this, new StoreRepository());
+
+            // url Store/TracksPage/{SortBy}/{OrderBy}
+            this.tracks = presenter.GetTracks(/*sortby, orderby*/);
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            trackContainer.DataSource = this.tracks;
+            trackContainer.DataBind();
         }
     }
 }
