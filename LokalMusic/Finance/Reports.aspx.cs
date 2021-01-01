abfd9fa4ce9b0a2e51231a2cc16c2efb3b6fe330
LipatJob <35028794+LipatJob@users.noreply.github.com>
2021-01-01 23:11:23 +0800

    started with finance module partial implementation of report module

---
+++ LokalMusic/Finance/Reports.aspx.cs
@@ -0,0 +1,46 @@
+﻿using LokalMusic._Code.Repositories.Finance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LokalMusic.Finance
+{
+    public partial class Reports : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        public static Object GetReportData(DateTime startDate, DateTime endDate, string frequency)
+        {
+            var repository = new ReportsRepository();
+
+            // validate frequency
+            string methodName = "";
+            if(frequency == "WEEKLY")
+            {
+                methodName = ReportsRepository.WEEKLY;
+            }
+            else if(frequency == "MONTHLY")
+            {
+                methodName = ReportsRepository.MONTHLY;
+            }
+            else if (frequency == "YEARLY")
+            {
+                methodName = ReportsRepository.YEARLY;
+            }
+            else
+            {
+                return new { errorMessage = "invalid frequency" };
+            }
+
+            return repository.GetReport(startDate, endDate, methodName);
+        }
+    }
+}
