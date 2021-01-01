d35b8c4b10e4291b33dd0c7bea9ef7bc7abbc15b
gn-grc <genejogarcia.gg@gmail.com>
2021-01-01 19:31:47 +0800

    prepared layout for albums page

--- LokalMusic/Store/AlbumsPage.aspx.cs
+++ LokalMusic/Store/AlbumsPage.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LokalMusic._Code.Presenters.Store;
+using LokalMusic._Code.Repositories;
+using LokalMusic._Code.Views.Store;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +10,13 @@ using System.Web.UI.WebControls;
 
 namespace LokalMusic.Store
 {
-    public partial class AlbumsPage : System.Web.UI.Page
+    public partial class AlbumsPage : System.Web.UI.Page, IAlbumsPage
     {
+        private AlbumsPagePresenter presenter;
+        public AlbumsPage()
+        {
+            this.presenter = new AlbumsPagePresenter(this, new StoreRepository());
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
 
