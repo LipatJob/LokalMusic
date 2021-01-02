e47f946f36ed3656ed82b086ce32d6e54c56ec74
gn-grc <genejogarcia.gg@gmail.com>
2021-01-02 22:27:59 +0800

    bind url to album and track view all

--- LokalMusic/Store/Home.aspx.designer.cs
+++ LokalMusic/Store/Home.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace LokalMusic.Store
         protected global::System.Web.UI.WebControls.Repeater albumContainer;
 
         /// <summary>
+        /// artistViewAll control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor artistViewAll;
+
+        /// <summary>
         /// artistContainer control.
         /// </summary>
         /// <remarks>
@@ -40,6 +49,15 @@ namespace LokalMusic.Store
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Repeater artistContainer;
+
+        /// <summary>
+        /// trackViewAll control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor trackViewAll;
 
         /// <summary>
         /// trackContainer control.
