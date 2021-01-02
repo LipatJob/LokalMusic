3a4e2f12204241921f8b541dcad8528cd17daa2a
gn-grc <genejogarcia.gg@gmail.com>
2021-01-02 20:05:10 +0800

    added new summary model to replace old models

---
+++ LokalMusic/_Code/Models/Store/TrackSummary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LokalMusic._Code.Models.Store
+{
+    public class TrackSummary
+    {
+        public int TrackId { get; set; }
+        public int AlbumId { get; set; }
+        public int ArtistId { get; set; }
+
+        public string TrackName { get; set; }
+        public Decimal Price { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string AlbumName { get; set; }
+        public string ArtistName { get; set; }
+
+        public string Genre { get; set; }
+
+        public string AudioClip { get; set; }
+        public TimeSpan AudioClipDuration { get; set; }
+        public TimeSpan AudioDuration { get; set; }
+
+        public string AlbumCover { get; set; }
+
+    }
+}
