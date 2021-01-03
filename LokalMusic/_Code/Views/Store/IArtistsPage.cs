6946dccdd1dcb3b357546ecabdcd7a41cf823a1a
gn-grc <genejogarcia.gg@gmail.com>
2021-01-03 20:04:35 +0800

    added new files for artists page

---
+++ LokalMusic/_Code/Views/Store/IArtistsPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LokalMusic._Code.Views.Store
+{
+    public interface IArtistsPage
+    {
+    }
+}
