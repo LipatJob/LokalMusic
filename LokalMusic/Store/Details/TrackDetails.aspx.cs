6971e8fa85f5c08ad17152aeacb7808560b28d79
gn-grc <genejogarcia.gg@gmail.com>
2021-01-06 21:25:45 +0800

    added template for track display details

---
+++ LokalMusic/Store/Details/TrackDetails.aspx.cs
@@ -0,0 +1,26 @@
+﻿using LokalMusic._Code.Presenters.Store.Details;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LokalMusic.Store.Details
+{
+    public partial class TrackDetails : System.Web.UI.Page
+    {
+        private TrackDetailsPresenter presenter;
+        public TrackDetails()
+        {
+            this.presenter = new TrackDetailsPresenter();
+        }
+
+        protected string test = "123123";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
