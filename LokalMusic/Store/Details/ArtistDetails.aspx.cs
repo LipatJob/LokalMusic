ff48b217d01724246b92e1c456cdb722ae94498e
gn-grc <genejogarcia.gg@gmail.com>
2021-01-06 22:06:52 +0800

    added template for artist details display but list of album not yet prepared

---
+++ LokalMusic/Store/Details/ArtistDetails.aspx.cs
@@ -0,0 +1,26 @@
+﻿using LokalMusic._Code.Presenters.Store.Details;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LokalMusic.Store.Details
+{
+    public partial class ArtistDetails : System.Web.UI.Page
+    {
+
+        protected ArtistDetailsPresenter presenter;
+
+        public ArtistDetails()
+        {
+            this.presenter = new ArtistDetailsPresenter();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
