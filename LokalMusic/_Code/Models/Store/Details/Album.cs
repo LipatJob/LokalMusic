bcf111093461cf23a31ae3d42d8c0c4ea3a179cc
gn-grc <genejogarcia.gg@gmail.com>
2021-01-07 22:07:59 +0800

    added model for track, album, and artist details

---
+++ LokalMusic/_Code/Models/Store/Details/Album.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LokalMusic._Code.Models.Store.Details
+{
+    public class Album
+    {
+        public Album(int albumId, int artistId, string albumName, decimal price, string description, DateTime releaseDate, string albumCover, string artistName)
+        {
+            AlbumId = albumId;
+            ArtistId = artistId;
+            AlbumName = albumName;
+            Price = price;
+            Description = description;
+            ReleaseDate = releaseDate;
+            AlbumCover = albumCover;
+            ArtistName = artistName;
+        }
+
+        public int AlbumId { get; set; }
+        public int ArtistId { get; set; }
+
+        public string AlbumName { get; set; }
+        public Decimal Price { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public string AlbumCover { get; set; }
+
+        public string ArtistName { get; set; }
+
+        // processed in presenter
+        public string Genres { get; set; }
+        public int TrackCount { get; set; }
+        public int MinuteCount { get; set; }
+    }
+}
--- LokalMusic/_Code/Models/Store/Details/Track.cs
+++ LokalMusic/_Code/Models/Store/Details/Track.cs
@@ -7,6 +7,44 @@ namespace LokalMusic._Code.Models.Store.Details
 {
     public class Track
     {
+        public Track(int trackId, int albumId, int artistId, string trackName, decimal price, TimeSpan audioDuration, string audioAddress, DateTime dateAdded, string description, string albumName, string artistName, DateTime albumReleaseDate, string albumCover)
+        {
+            TrackId = trackId;
+            AlbumId = albumId;
+            ArtistId = artistId;
+            TrackName = trackName;
+            Price = price;
+            AudioDuration = audioDuration;
+            AudioAddress = audioAddress;
+            DateAdded = dateAdded;
+            Description = description;
+            AlbumName = albumName;
+            ArtistName = artistName;
+            AlbumReleaseDate = albumReleaseDate;
+            AlbumCover = albumCover;
+        }
+
+        public int TrackId { get; set; }
+        public int AlbumId { get; set; }
+        public int ArtistId { get; set; }
+
+        public string TrackName { get; set; }
+        public Decimal Price { get; set; }
+        public TimeSpan AudioDuration { get; set; }
+        public string AudioAddress { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        public string Description { get; set; }
+
+        public string AlbumName { get; set; }
+        public string ArtistName { get; set; }
+        public DateTime AlbumReleaseDate { get; set; }
+
+        public string AlbumCover { get; set; }
+
+        // processed in presenter
+        public string Genre { get; set; }
+
 
     }
 }
