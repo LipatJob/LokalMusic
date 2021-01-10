7994225faa63bd49109b0d249cd194ae38640028
gn-grc <genejogarcia.gg@gmail.com>
2021-01-10 21:36:14 +0800

    album data bind on album details page

--- LokalMusic/_Code/Presenters/Store/Details/AlbumDetailsPresenter.cs
+++ LokalMusic/_Code/Presenters/Store/Details/AlbumDetailsPresenter.cs
@@ -1,4 +1,5 @@
-﻿using LokalMusic._Code.Repositories.Store.ProductDetails;
+﻿using LokalMusic._Code.Models.Store.Details;
+using LokalMusic._Code.Repositories.Store.ProductDetails;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,11 @@ namespace LokalMusic._Code.Presenters.Store.Details
         public AlbumDetailsPresenter(ProductDetailsRepository repo)
         {
             this.repository = repo;
+        }
+
+        public Album GetAlbumDetails(int albumId, int artistId)
+        {
+            return this.repository.GetAlbumDetails(albumId, artistId);
         }
     }
 }
