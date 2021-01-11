8bf4df6eeb460eab106bad6c9535ae3188e21379
gn-grc <genejogarcia.gg@gmail.com>
2021-01-11 21:57:34 +0800

    implemented url request handler in album details

--- LokalMusic/Store/Details/AlbumDetails.aspx.cs
+++ LokalMusic/Store/Details/AlbumDetails.aspx.cs
@@ -1,4 +1,5 @@
-﻿using LokalMusic._Code.Models.Store.Details;
+﻿using LokalMusic._Code.Helpers;
+using LokalMusic._Code.Models.Store.Details;
 using LokalMusic._Code.Presenters.Store.Details;
 using LokalMusic._Code.Repositories.Store.ProductDetails;
 using System;
@@ -20,10 +21,48 @@ namespace LokalMusic.Store.Details
         {
             this.presenter = new AlbumDetailsPresenter(new ProductDetailsRepository());
 
-            //urk
-            this.albumDetails = this.presenter.GetAlbumDetails(4, 6);
-            this.tracks = this.presenter.GetTracksOfAlbum(4, 6);
+            this.HandleUrlRequest();
+        }
 
+        private void HandleUrlRequest()
+        {
+            List<string> urlParams = new List<string>();
+            List<int> parsedParams = new List<int>();
+
+            urlParams.Add((string)NavigationHelper.GetRouteValue("AlbumId"));
+            urlParams.Add((string)NavigationHelper.GetRouteValue("ArtistId"));
+
+            if (urlParams.Count != 2) { this.InvalidRequest(); }
+            else
+            {
+                int tempParam = 0;
+                foreach (string param in urlParams)
+                {
+                    int.TryParse(param, out tempParam);
+                    parsedParams.Add(tempParam);
+
+                    tempParam = 0;
+                }
+            }
+
+            // call presenter to update model
+            this.albumDetails = this.presenter.GetAlbumDetails(
+                parsedParams[0],
+                parsedParams[1]
+                );
+
+            if (this.albumDetails == null)
+                this.InvalidRequest();
+            else 
+             this.tracks = this.presenter.GetTracksOfAlbum(
+                 parsedParams[0],
+                 parsedParams[1]
+                 );
+        }
+
+        private void InvalidRequest()
+        {
+            NavigationHelper.Redirect("~/Store/AlbumsPage.aspx");
         }
 
         protected void Page_Load(object sender, EventArgs e)
