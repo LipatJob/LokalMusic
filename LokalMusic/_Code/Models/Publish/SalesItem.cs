509f1b9b63e798a0511e60351d74f3d1f8e76927
charmaine-rabano <charmaine.rabano@gmail.com>
2021-01-13 17:38:35 +0800

    implemented sales history page

---
+++ LokalMusic/_Code/Models/Publish/SalesItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LokalMusic._Code.Models.Publish
+{
+    public class SalesItem
+    {
+        public int TransactionID { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Customer { get; set; }
+
+        public string Products { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+    }
+}
---
+++ LokalMusic/_Code/Presenters/Publish/SalesPresenter.cs
@@ -0,0 +1,45 @@
+﻿using LokalMusic._Code.Helpers;
+using LokalMusic._Code.Models.Publish;
+using LokalMusic._Code.Repositories.Publish;
+using LokalMusic.Publish;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LokalMusic._Code.Presenters.Publish
+{
+    public class SalesPresenter
+    {
+        private Sales sales;
+        private SalesRepository salesRepository;
+
+        public SalesPresenter(Sales sales, SalesRepository salesRepository)
+        {
+            this.sales = sales;
+            this.salesRepository = salesRepository;
+        }
+
+        public void PageLoad()
+        {
+            if (AuthenticationHelper.LoggedIn == false)
+            {
+                NavigationHelper.Redirect("~/Account/Login");
+            }
+            else if (AuthenticationHelper.UserType != AuthenticationHelper.ARTIST_USER_TYPE)
+            {
+                NavigationHelper.Redirect("~");
+            }
+        }
+
+        public SalesModel GetSalesModel()
+        {
+            return new SalesModel()
+            {
+                ArtistName = salesRepository.GetArtistName(AuthenticationHelper.UserId),
+                SalesItems = salesRepository.GetSalesItems(AuthenticationHelper.UserId)
+            };
+        }
+
+    }
+}
