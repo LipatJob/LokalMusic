3dc8bd6a1fe9365d4dd38c56cf1de5e95e3adb2c
charmaine-rabano <charmaine.rabano@gmail.com>
2021-01-17 14:12:57 +0800

    implemented publish/albums page

--- LokalMusic/_Code/Presenters/Publish/AlbumsPresenter.cs
+++ LokalMusic/_Code/Presenters/Publish/AlbumsPresenter.cs
@@ -1,4 +1,5 @@
-﻿using LokalMusic._Code.Models.Publish;
+﻿using LokalMusic._Code.Helpers;
+using LokalMusic._Code.Models.Publish;
 using LokalMusic._Code.Repositories.Publish;
 using LokalMusic.Publish;
 using System;
@@ -21,24 +22,10 @@ namespace LokalMusic._Code.Presenters
 
         public AlbumsModel GetAlbumsModel()
         {
-            var items = new List<AlbumsItem>();
-            items.Add(new AlbumsItem()
-            {
-                AlbumCoverLink = @"~\Content\Images\default_cover.jpg",
-                AlbumName = "Test Album",
-                DateAdded = DateTime.Now,
-                TrackCount = 9,
-                Producer = "Test Producer",
-                SalesCount = 12,
-                Price = 100.00m,
-                TracksURL = "",
-                EditURL = ""
-            });
-
             return new AlbumsModel()
             {
-                ArtistName = "Job Lipat",
-                AlbumsItems = items
+                ArtistName = repository.GetArtistName(AuthenticationHelper.UserId),
+                AlbumsItems = repository.GetAlbumsItems(AuthenticationHelper.UserId)
             };
         }
     }
--- LokalMusic/_Code/Repositories/Publish/AlbumsRepository.cs
+++ LokalMusic/_Code/Repositories/Publish/AlbumsRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using LokalMusic._Code.Helpers;
+using LokalMusic._Code.Models.Publish;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +10,56 @@ namespace LokalMusic._Code.Repositories.Publish
 {
     public class AlbumsRepository
     {
+        public IList<AlbumsItem> GetAlbumsItems(int artistId)
+        {
+            string query = @"
+SELECT
+FileInfo.FileName AS AlbumCoverLink,
+Product.ProductName AS AlbumName,
+Product.DateAdded,
+Album.ProducerName AS Producer,
+Product.Price
+FROM Product
+RIGHT JOIN Album ON
+Product.ProductId = Album.AlbumId
+LEFT JOIN FileInfo ON
+Album.AlbumCoverID = FileInfo.FileId
+WHERE Album.UserId = @ArtistId
+";
+
+            var result = DbHelper.ExecuteDataTableQuery(query, ("ArtistId", artistId));
+
+            var Items = new List<AlbumsItem>();
+            foreach (DataRow row in result.Rows)
+            {
+                Items.Add(new AlbumsItem() {
+                    AlbumCoverLink = (string)row["AlbumCoverLink"],
+                    AlbumName = (string)row["AlbumName"],
+                    DateAdded = (DateTime)row["DateAdded"],
+                    TrackCount = 9,
+                    Producer = (string)row["Producer"],
+                    SalesCount = 12,
+                    Price = (decimal)row["Price"],
+                    TracksURL = "~",
+                    EditURL = "~"
+                });
+            }
+
+            return Items;
+        }
+
+        public string GetArtistName(int artistId)
+        {
+            string query = "SELECT ArtistName FROM ArtistInfo WHERE UserId = @ArtistId;";
+            var result = DbHelper.ExecuteDataTableQuery(query, ("ArtistId", artistId));
+
+            var artistName = "";
+            foreach (DataRow row in result.Rows)
+            {
+                artistName = (string)row["ArtistName"];
+            }
+
+            return artistName;
+        }
     }
 }
