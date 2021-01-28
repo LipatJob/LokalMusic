88c1b2c00ce6baf567ce0178b02c3e1450af3c14
gn-grc <genejogarcia.gg@gmail.com>
2021-01-28 14:53:32 +0800

    added models for cart page

---
+++ LokalMusic/_Code/Models/Cart/CartArtist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LokalMusic._Code.Models.Cart
+{
+    public class CartArtist
+    {
+
+        public int ArtistId { get; set; }
+
+        public string ArtistName { get; set; }
+        public List<CartTrack> tracks { get; set; }
+
+
+    }
+}
