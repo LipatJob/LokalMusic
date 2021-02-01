ea84fc55d324bdcac52606e850847c5acacd2c4a
gn-grc <genejogarcia.gg@gmail.com>
2021-02-01 16:11:02 +0800

    implemented payment information validations, and payment provider name passed in web service ajax call

--- LokalMusic/Template/StoreLayout.master.designer.cs
+++ LokalMusic/Template/StoreLayout.master.designer.cs
@@ -15,24 +15,6 @@ namespace LokalMusic.Template
     {
 
         /// <summary>
-        /// SearchTxt control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox SearchTxt;
-
-        /// <summary>
-        /// SearchBtn control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button SearchBtn;
-
-        /// <summary>
         /// navbarDropdown control.
         /// </summary>
         /// <remarks>
