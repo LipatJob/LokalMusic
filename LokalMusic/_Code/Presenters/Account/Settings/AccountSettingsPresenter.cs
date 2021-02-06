5ef1deddac847061d4a5f945e34d1e2d1f774d63
LipatJob <35028794+LipatJob@users.noreply.github.com>
2021-02-06 15:02:05 +0800

    implemented new settings UI

---
+++ LokalMusic/_Code/Presenters/Account/Settings/AccountSettingsPresenter.cs
@@ -0,0 +1,40 @@
+﻿using LokalMusic._Code.Helpers;
+using LokalMusic._Code.Models.Account;
+using LokalMusic._Code.Repositories.Account;
+using LokalMusic._Code.Views.Account;
+using LokalMusic._Code.Views.Account.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LokalMusic._Code.Presenters.Account
+{
+    public class AccountSettingsPresenter
+    {
+        private readonly IAccountSettingsViewModel model;
+        private readonly SettingsRepository repository;
+
+        public AccountSettingsPresenter(IAccountSettingsViewModel model, SettingsRepository repository)
+        {
+            this.model = model;
+            this.repository = repository;
+        }
+
+        public void PageLoad()
+        {
+
+            repository.GetAccountDetails(AuthenticationHelper.UserId, model);
+        }
+
+        public void ChangePassword()
+        {
+            repository.UpdatePassword(AuthenticationHelper.UserId, model.NewPassword);
+            NavigationHelper.Redirect("~/Account/Settings?PasswordChanged=True");
+        }
+
+        public bool CheckOldPassword()
+        {
+            return repository.CheckPassword(AuthenticationHelper.UserId, model.OldPassword);
+        }
+    }
+}
--- LokalMusic/_Code/Repositories/Account/SettingsRepository.cs
+++ LokalMusic/_Code/Repositories/Account/SettingsRepository.cs
@@ -1,5 +1,6 @@
 ﻿using LokalMusic._Code.Helpers;
 using LokalMusic._Code.Models.Account;
+using LokalMusic._Code.Models.Account.Settings;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -9,59 +10,70 @@ using System.Web;
 
 namespace LokalMusic._Code.Repositories.Account
 {
-	public class SettingsRepository
-	{
-		public void GetUserDetails(int userId, ISettingsModel model)
-		{
-			string detailsQuery = @"
+    public class SettingsRepository
+    {
+        public void GetAccountDetails(int userId, IAccountSettingsModel model)
+        {
+            string detailsQuery = @"
+SELECT
+	Email,
+	Username
+FROM UserInfo
+WHERE [UserInfo].UserId = @UserId;
+";
+            var query = DbHelper.ExecuteDataTableQuery(detailsQuery, ("UserId", userId));
+            model.Email = (string)query.Rows[0]["Email"];
+            model.Username = (string)query.Rows[0]["Username"];
+
+        }
+
+        public void GetProfileDetails(int userId, IProfileSettingsModel model)
+        {
+            string detailsQuery = @"
 SELECT
 	FileName,
-	Email,
-	Username,
 	COALESCE(ArtistInfo.Bio, '') AS Bio,
 	COALESCE(ArtistInfo.ArtistName, '') AS ArtistName
 FROM UserInfo
 	LEFT JOIN [ArtistInfo] ON [ArtistInfo].UserId = [UserInfo].UserId
 	LEFT JOIN [FileInfo] ON [UserInfo].ProfileImageId = [FileInfo].FileId WHERE [UserInfo].UserId = @UserId;
 ";
-			var query = DbHelper.ExecuteDataTableQuery(detailsQuery, ("UserId", userId));
-			if (query.Rows[0].IsNull("FileName"))
-			{
-				model.ProfileImage = "~/Content/Images/Logo.png";
-			}
-			else
-			{
-				model.ProfileImage = query.Rows[0]["FileName"].ToString();
-			}
-			model.Email = (string)query.Rows[0]["Email"];
-			model.Username = (string)query.Rows[0]["Username"];
-			model.ArtistBio = (string)query.Rows[0]["Bio"];
-			model.ArtistName = (string)query.Rows[0]["ArtistName"];
 
-		}
+            var query = DbHelper.ExecuteDataTableQuery(detailsQuery, ("UserId", userId));
+            if (query.Rows[0].IsNull("FileName"))
+            {
+                model.ProfileImage = "~/Content/Images/Old Logo.png";
+            }
+            else
+            {
+                model.ProfileImage = query.Rows[0]["FileName"].ToString();
+            }
+            model.ArtistBio = (string)query.Rows[0]["Bio"];
+            model.ArtistName = (string)query.Rows[0]["ArtistName"];
+        }
 
-		public void UpdatePassword(int userId, string newPassword)
-		{
-			string updatePasswordQuery = "UPDATE UserInfo SET Password = @Password WHERE UserId = @UserId";
-			DbHelper.ExecuteNonQuery(updatePasswordQuery, ("Password", newPassword), ("UserId", userId));
-		}
-
-		public bool CheckPassword(int userId, string password)
-		{
-			string query = "SELECT UserId FROM UserInfo WHERE UserId = @UserId AND Password = @Password";
-			return DbHelper.ExecuteScalar(query, ("UserId", userId), ("Password", password)) != null;
-		}
-
-		public void UpdateArtistBio(int userId, string bio)
+        public void UpdatePassword(int userId, string newPassword)
         {
-			string updateBioQuery = "UPDATE ArtistInfo SET Bio = @Bio WHERE UserId = @UserId";
-			DbHelper.ExecuteNonQuery(updateBioQuery, ("Bio", bio), ("UserId", userId));
+            string updatePasswordQuery = "UPDATE UserInfo SET Password = @Password WHERE UserId = @UserId";
+            DbHelper.ExecuteNonQuery(updatePasswordQuery, ("Password", newPassword), ("UserId", userId));
+        }
 
-		}
+        public bool CheckPassword(int userId, string password)
+        {
+            string query = "SELECT UserId FROM UserInfo WHERE UserId = @UserId AND Password = @Password";
+            return DbHelper.ExecuteScalar(query, ("UserId", userId), ("Password", password)) != null;
+        }
 
-		public IList<PaymentHistoryItem> GetPaymentHistory(int? userId)
-		{
-			string productNameQuery = @"
+        public void UpdateArtistBio(int userId, string bio)
+        {
+            string updateBioQuery = "UPDATE ArtistInfo SET Bio = @Bio WHERE UserId = @UserId";
+            DbHelper.ExecuteNonQuery(updateBioQuery, ("Bio", bio), ("UserId", userId));
+
+        }
+
+        public IList<PaymentHistoryItem> GetPaymentHistory(int? userId)
+        {
+            string productNameQuery = @"
 SELECT
 	[OrderInfo].OrderId AS OrderId,
 	MAX([OrderInfo].OrderDate) AS OrderDate,
@@ -76,59 +88,59 @@ WHERE [OrderInfo].CustomerId = @UserId
 GROUP BY [OrderInfo].OrderId
 ORDER BY OrderDate DESC;
 ";
-			var result = DbHelper.ExecuteDataTableQuery(productNameQuery, ("UserId", userId));
+            var result = DbHelper.ExecuteDataTableQuery(productNameQuery, ("UserId", userId));
 
-			return result.AsEnumerable().Select((row) =>
-			{
-				return new PaymentHistoryItem()
-				{
-					TransactionId = (int)row["OrderId"],
-					TransactionDate = (DateTime)row["OrderDate"],
-					ItemsPurchased = (string)row["ItemsPurchased"],
-					Amount = (decimal)row["Amount"]
-				};
-			}).ToList();
-		}
+            return result.AsEnumerable().Select((row) =>
+            {
+                return new PaymentHistoryItem()
+                {
+                    TransactionId = (int)row["OrderId"],
+                    TransactionDate = (DateTime)row["OrderDate"],
+                    ItemsPurchased = (string)row["ItemsPurchased"],
+                    Amount = (decimal)row["Amount"]
+                };
+            }).ToList();
+        }
 
-		public void ChangeProfilePicture(int userId, HttpPostedFile file)
-		{
-			string fileName = userId + Path.GetExtension(file.FileName);
-			if (HasProfilePicture(userId))
-			{
-				UpdateProfilePicture(userId, fileName, file);
-			}
-			else
-			{
-				CreateNewProfilePicture(userId, fileName, file);
-			}
-		}
+        public void ChangeProfilePicture(int userId, HttpPostedFile file)
+        {
+            string fileName = userId + Path.GetExtension(file.FileName);
+            if (HasProfilePicture(userId))
+            {
+                UpdateProfilePicture(userId, fileName, file);
+            }
+            else
+            {
+                CreateNewProfilePicture(userId, fileName, file);
+            }
+        }
 
-		private void CreateNewProfilePicture(int userId, string fileName, HttpPostedFile file)
-		{
-			string fileLocation = FileSystemHelper.UploadFile(fileName, FileSystemHelper.PICTURE_CONTAINER_NAME, file, true);
-			int profileImageId = (int)DbHelper.ExecuteScalar(
-				"INSERT INTO FileInfo(FileTypeId, FileName) VALUES((SELECT FileTypeId FROM FileType WHERE FileTypeName=@FileTypeName), @FileName) SELECT SCOPE_IDENTITY()",
-				("FileTypeName", FileSystemHelper.PICTURE_CONTAINER_NAME),
-				("FileName", fileLocation));
-			DbHelper.ExecuteNonQuery(
-				"UPDATE UserInfo SET ProfileImageId=@ProfileImageId WHERE UserId=@UserId",
-				("ProfileImageId", profileImageId),
-				("UserId", userId));
-		}
+        private void CreateNewProfilePicture(int userId, string fileName, HttpPostedFile file)
+        {
+            string fileLocation = FileSystemHelper.UploadFile(fileName, FileSystemHelper.PICTURE_CONTAINER_NAME, file, true);
+            int profileImageId = (int)DbHelper.ExecuteScalar(
+                "INSERT INTO FileInfo(FileTypeId, FileName) VALUES((SELECT FileTypeId FROM FileType WHERE FileTypeName=@FileTypeName), @FileName) SELECT SCOPE_IDENTITY()",
+                ("FileTypeName", FileSystemHelper.PICTURE_CONTAINER_NAME),
+                ("FileName", fileLocation));
+            DbHelper.ExecuteNonQuery(
+                "UPDATE UserInfo SET ProfileImageId=@ProfileImageId WHERE UserId=@UserId",
+                ("ProfileImageId", profileImageId),
+                ("UserId", userId));
+        }
 
-		private void UpdateProfilePicture(int userId, string fileName, HttpPostedFile file)
-		{
-			string fileLocation = FileSystemHelper.UploadFile(fileName, FileSystemHelper.PICTURE_CONTAINER_NAME, file, true);
-			DbHelper.ExecuteNonQuery(
-				"UPDATE FileInfo SET FileName = @FileName WHERE FileId = (SELECT ProfileImageId FROM UserInfo WHERE UserId=@UserId)",
-				("FileName", fileLocation),
-				("UserId", userId));
-		}
+        private void UpdateProfilePicture(int userId, string fileName, HttpPostedFile file)
+        {
+            string fileLocation = FileSystemHelper.UploadFile(fileName, FileSystemHelper.PICTURE_CONTAINER_NAME, file, true);
+            DbHelper.ExecuteNonQuery(
+                "UPDATE FileInfo SET FileName = @FileName WHERE FileId = (SELECT ProfileImageId FROM UserInfo WHERE UserId=@UserId)",
+                ("FileName", fileLocation),
+                ("UserId", userId));
+        }
 
-		private bool HasProfilePicture(int userId)
-		{
-			string query = "SELECT ProfileImageId FROM UserInfo WHERE UserId = @UserId";
-			return DbHelper.ExecuteScalar(query, ("UserId", userId)) != null;
-		}
-	}
+        private bool HasProfilePicture(int userId)
+        {
+            string query = "SELECT ProfileImageId FROM UserInfo WHERE UserId = @UserId";
+            return DbHelper.ExecuteScalar(query, ("UserId", userId)) != null;
+        }
+    }
 }
