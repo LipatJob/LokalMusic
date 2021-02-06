51f609f373ca2fe772755b92554fa9f66d4b2cfc
gn-grc <genejogarcia.gg@gmail.com>
2021-02-06 20:59:36 +0800

    used bootstrap icons for search. and FIXED- error when search values ends with ".".

--- LokalMusic/Template/StoreLayout.master.cs
+++ LokalMusic/Template/StoreLayout.master.cs
@@ -12,8 +12,12 @@ namespace LokalMusic.Template
 
         protected void SearchBtn_Click(object sender, EventArgs e)
         {
-            if(SearchTxt.Value != "")
+            if (SearchTxt.Value != null)
             {
+                // remove periods at the end of the string
+                if (SearchTxt.Value.EndsWith("."))
+                    SearchTxt.Value = SearchTxt.Value.Substring(0, SearchTxt.Value.Length - 1);
+
                 string searchUrl = NavigationHelper.CreateAbsoluteUrl($"/Store/Search/{SearchTxt.Value}");
                 NavigationHelper.Redirect(searchUrl);
             }
