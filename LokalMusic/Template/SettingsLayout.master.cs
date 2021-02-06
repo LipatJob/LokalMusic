c8227e4c1720ba1bc33f3f05178315e89c9d8dfa
LipatJob <35028794+LipatJob@users.noreply.github.com>
2021-02-06 15:02:05 +0800

    implemented new settings UI

--- LokalMusic/Template/SettingsLayout.master.cs
+++ LokalMusic/Template/SettingsLayout.master.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LokalMusic._Code.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,10 @@ namespace LokalMusic.Template
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (AuthenticationHelper.LoggedIn == false)
+            {
+                NavigationHelper.RedirectReturnAddress("~/Account/Login");
+            }
         }
     }
 }
---
+++ LokalMusic/_Code/Presenters/Account/Settings/ProfileSettingsPresenter.cs
@@ -0,0 +1,40 @@
+﻿using LokalMusic._Code.Helpers;
+using LokalMusic._Code.Models.Account;
+using LokalMusic._Code.Repositories.Account;
+using LokalMusic._Code.Views.Account;
+using LokalMusic._Code.Views.Account.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LokalMusic._Code.Presenters.Account
+{
+    public class ProfileSettingsPresenter
+    {
+        private readonly IProfileSettingsViewModel viewModel;
+        private readonly SettingsRepository repository;
+
+        public ProfileSettingsPresenter(IProfileSettingsViewModel viewModel, SettingsRepository repository)
+        {
+            this.viewModel = viewModel;
+            this.repository = repository;
+        }
+
+        public void PageLoad()
+        {
+            repository.GetProfileDetails(AuthenticationHelper.UserId, viewModel);
+        }
+
+        public void UpdateProfilePicture()
+        {
+            repository.ChangeProfilePicture(AuthenticationHelper.UserId, viewModel.UploadedProfilePicture);
+            NavigationHelper.Redirect("~/Account/Settings?ProfileImageChanged=True");
+        }
+
+        internal void UpdateBio()
+        {
+            repository.UpdateArtistBio(AuthenticationHelper.UserId, viewModel.ArtistBio);
+            NavigationHelper.Redirect("~/Account/Settings?ArtistBioChanged=True");
+        }
+    }
+}
