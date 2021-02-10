badb49fdb094f745fc4ff761c65c9b6a8b35ad1b
gn-grc <genejogarcia.gg@gmail.com>
2021-02-10 23:36:02 +0800

    added web services for track modal display

---
+++ LokalMusic/Store/TracksService.asmx.cs
@@ -0,0 +1,34 @@
+﻿using LokalMusic._Code.Models.Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace LokalMusic.Store
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public TrackSummary GetTrack(int trackId)
+        {
+            var a = trackId;
+
+            TrackSummary s = new TrackSummary();
+
+            s.TrackName = "Hellooooow";
+
+            return s;
+        }
+    }
+}
