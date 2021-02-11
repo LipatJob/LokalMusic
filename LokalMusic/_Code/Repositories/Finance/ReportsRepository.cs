3a5c8a29ff9504a5663e6b2dc3c5f21efe2a0671
LipatJob <35028794+LipatJob@users.noreply.github.com>
2021-02-12 02:08:25 +0800

    added max rows in report and reports now start at the start of the week/month/year

--- LokalMusic/_Code/Repositories/Finance/ReportsRepository.cs
+++ LokalMusic/_Code/Repositories/Finance/ReportsRepository.cs
@@ -16,6 +16,14 @@ namespace LokalMusic._Code.Repositories.Finance
         {
             if (new[] { WEEKLY, MONTHLY, YEARLY }.Contains(period) == false) { return null; }
 
+            start = EvaluateStartDate(start, period);
+            end = EvaluateEndDate(end, period);
+
+            if(DateRangeTooLarge(start, end, period))
+            {
+                return null;
+            }
+
             return new
             {
                 chart = GetChartValues(start, end, period),
@@ -23,17 +31,98 @@ namespace LokalMusic._Code.Repositories.Finance
             };
         }
 
+        private bool DateRangeTooLarge(DateTime start, DateTime end, string period)
+        {
+            TimeSpan span = end - start;
+            const int MONTH_DAYS = 30;
+            const int WEEK_DAYS = 7;
+            const int YEAR_DAYS = 365;
+            const int MAX_COUNT = 30;
+
+            if (period == MONTHLY)
+            {
+                if (span.TotalDays > MONTH_DAYS * MAX_COUNT)
+                {
+                    return true;
+                }
+            }
+            else if (period == WEEKLY)
+            {
+                if (span.TotalDays > WEEK_DAYS * MAX_COUNT)
+                {
+                    return true;
+                }
+            }
+            else if (period == YEARLY)
+            {
+                if (span.TotalDays > YEAR_DAYS * MAX_COUNT)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private DateTime EvaluateStartDate(DateTime start, string period)
+        {
+            if(period == MONTHLY)
+            {
+                return new DateTime(start.Year, start.Month, 1, 0, 0, 0);
+            }
+            else if(period == WEEKLY)
+            {
+                return StartOfWeek(start, DayOfWeek.Sunday);
+            }
+            else if(period == YEARLY)
+            {
+                return new DateTime(start.Year, 1, 1, 0, 0, 0);
+            }
+
+            return start;
+        }
+
+        private DateTime StartOfWeek(DateTime start, DayOfWeek startOfWeek)
+        {
+            int diff = (7 + (start.DayOfWeek - startOfWeek)) % 7;
+            return start.AddDays(-1 * diff).Date;
+        }
+
+        private DateTime EvaluateEndDate(DateTime end, string period)
+        {
+            if (period == MONTHLY)
+            {
+                return new DateTime(end.Year, end.Month, 1);
+            }
+            else if (period == WEEKLY)
+            {
+                return EndOfWeek(end);
+            }
+            else if (period == YEARLY)
+            {
+                return new DateTime(end.Year, 12, DateTime.DaysInMonth(end.Year, 12));
+            }
+
+            return end;
+        }
+
+        private DateTime EndOfWeek(DateTime end)
+        {
+            return StartOfWeek(end, DayOfWeek.Sunday).AddDays(6);
+        }
+
         private object GetChartValues(DateTime start, DateTime end, string period)
         {
             string query =
 $@"
 SELECT
-	[Months].DateStart AS Date,
-	SUM([OrderInfo].AmountPaid) AS TotalAmountUserPaid
+	CONVERT(VARCHAR(20), [Months].DateStart, 107) + ' - ' +  CONVERT(VARCHAR(20), MAX([Months].DateEnd), 107) AS Date,
+	COALESCE(SUM([OrderInfo].AmountPaid), 0) AS TotalAmountUserPaid
 FROM {period}(@StartDate, @EndDate) AS Months
 LEFT JOIN [OrderInfo] ON
 	[Months].DateStart < = [OrderInfo].OrderDate AND
 	[Months].DateEnd > [OrderInfo].OrderDate
+WHERE [Months].DateStart <= @EndDate
 GROUP BY [Months].DateStart
 ";
             List<string> labels = new List<string>();
@@ -45,13 +134,18 @@ GROUP BY [Months].DateStart
 
             foreach (var row in values.AsEnumerable())
             {
-                labels.Add(((DateTime)row["Date"]).ToShortDateString());
+                labels.Add(((string) row["Date"]));
                 data.Add(row.IsNull("TotalAmountUserPaid") ? 0 : (decimal)row["TotalAmountUserPaid"]);
             }
+
+            string startString = start.ToString();
+            string endString = end.ToString();
             return new
             {
                 labels,
-                data
+                data,
+                startString,
+                endString
             };
         }
 
