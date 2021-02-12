21f2c7aaf21872a525f3d84fc761117eb6fcfe0e
gn-grc <genejogarcia.gg@gmail.com>
2021-02-12 18:33:05 +0800

    implemented fan/playlist

---
+++ LokalMusic/_Code/Models/Fan/PlaylistTrack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LokalMusic._Code.Models.Fan
+{
+    public class PlaylistTrack
+    {
+
+        public int TrackId { get; set; }
+        public string TrackName { get; set; }
+        public string TrackAudio { get; set; }
+
+    }
+}
